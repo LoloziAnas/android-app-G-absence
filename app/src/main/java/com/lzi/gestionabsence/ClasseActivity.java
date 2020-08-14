8c81f61f5fc8bec5a763bf3152c91ef4d3e75324
LoloziAnas <lolozianas1@gmail.com>
2020-08-14 05:16:15 +0100

    initial commit :tada:

--- app/src/main/java/com/lzi/gestionabsence/ClasseActivity.java
+++ app/src/main/java/com/lzi/gestionabsence/ClasseActivity.java
@@ -4,20 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.lzi.gestionabsence.DAO.ClasseDAO;
 import com.lzi.gestionabsence.adapters.ClasseAdapter;
-import com.lzi.gestionabsence.api.Constants;
 import com.lzi.gestionabsence.entities.Classe;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
